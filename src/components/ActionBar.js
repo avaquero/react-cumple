eed7413d60d30a27d246fdfd1f468fdf2b65acff
avaquero <avaquero1469@gmail.com>
2020-08-03 20:30:51 +0200

    Clase 76

--- src/components/ActionBar.js
+++ src/components/ActionBar.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import firebase from "../utils/firebase"
 
-export default function ActionBar() {
+export default function ActionBar(props) {
+
+    const { showList, setShowList } = props;
+
     return (
         <View style={styles.viewFooter}>
             <View style={styles.viewClose}>
-                <Text style={styles.text}>Cerrar sesión</Text>
+                <Text style={styles.text} onPress={() => firebase.auth().signOut()}>
+                    Cerrar sesión
+                    </Text>
             </View>
             <View style={styles.viewAdd}>
-                <Text style={styles.text}>Nueva fecha</Text>
+                <Text style={styles.text} onPress={() => setShowList(!showList)}>
+                    {showList ? "Nueva fecha" : "Cancelar fecha"}
+                </Text>
             </View>
         </View>
     )
--- src/components/ListBirthday.js
+++ src/components/ListBirthday.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import ActionBar from "./ActionBar";
+import AddBirthday from "./AddBirthday"
+import ActionBar from "./ActionBar"
 
 export default function ListBirthday() {
+
+    const [showList, setShowList] = useState(true);
+
     return (
         <View style={styles.container}>
-            <Text>Hola Mundo</Text>
-            <ActionBar />
+            {showList ? (
+                <>
+                    <Text>LIST</Text>
+                    <Text>LIST</Text>
+                    <Text>LIST</Text>
+                    <Text>LIST</Text>
+                    <Text>LIST</Text>
+                    <Text>LIST</Text>
+                    <Text>LIST</Text>
+                </>
+            ) : (
+                    <AddBirthday />
+                )}
+
+            <ActionBar showList={showList} setShowList={setShowList} />
         </View>
     )
 }
