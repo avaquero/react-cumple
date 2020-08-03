c3ce29b561f6219cf7aaba07fcc7ff20d9b2b06a
avaquero <avaquero1469@gmail.com>
2020-08-03 21:01:48 +0200

    Clase 78

--- src/components/AddBirthday.js
+++ src/components/AddBirthday.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TextInput } from 'react-native'
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default function AddBirthday() {
+    const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+
+
+    const hideDatePicker = () => {
+        setIsDatePickerVisible(false);
+    }
+
+    const showDatePicker = () => {
+        setIsDatePickerVisible(true);
+    }
+
+    const handlerConfirm = (date) => {
+        console.log(date);
+        hideDatePicker();
+    }
+
     return (
         <>
             <View style={styles.container}>
@@ -15,7 +32,16 @@ export default function AddBirthday() {
                     placeholder="Apellidos"
                     placeholderTextColor="#969696"
                 />
+                <View style={[styles.input, styles.datePicker]}>
+                    <Text style={styles.textDate} onPress={showDatePicker}>Fecha de nacimiento</Text>
+                </View>
             </View>
+            <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handlerConfirm}
+                onCancel={hideDatePicker}
+            />
         </>
     )
 }
@@ -38,5 +64,12 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         borderWidth: 1,
         borderColor: "#1e3040",
-    }
+    },
+    datePicker: {
+        justifyContent: "center",
+    },
+    textDate: {
+        color: "#969696",
+        fontSize: 18,
+    },
 })
