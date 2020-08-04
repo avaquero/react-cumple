59ef7ab9150a70ce0ae2be48142994737e68fdb1
avaquero <avaquero1469@gmail.com>
2020-08-04 12:09:33 +0200

    Clase 85

--- src/components/ListBirthday.js
+++ src/components/ListBirthday.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import AddBirthday from "./AddBirthday"
 import ActionBar from "./ActionBar"
+import firebase from "../utils/firebase"
+import "firebase/firestore"
+
+firebase.firestore().settings({ experimentalForceLongPolling: true });
+const db = firebase.firestore(firebase);
 
 export default function ListBirthday(props) {
     const { user } = props;
     const [showList, setShowList] = useState(true);
+
+    const [birthday, setBirthday] = useState({});
+
+    useEffect(() => {
+        setBirthday([]);
+        db.collection(user.uid)
+            .orderBy("dateBirth", "asc")
+            .get()
+            .then((response) => {
+                const itemsArray = [];
+                response.forEach((doc) => {
+                    const data = doc.data();
+                    data.id = doc.id;
+                    itemsArray.push(data);
+                });
+                setBirthday(itemsArray);
+            });
+    }, []);
 
     return (
         <View style={styles.container}>
