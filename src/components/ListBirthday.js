c3c21c6e26b774bc92caa86d74f0aea2c6deca44
avaquero <avaquero1469@gmail.com>
2020-08-04 17:09:34 +0200

    Clase 86

--- src/components/ListBirthday.js
+++ src/components/ListBirthday.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import moment from "moment"
 import AddBirthday from "./AddBirthday"
 import ActionBar from "./ActionBar"
 import firebase from "../utils/firebase"
@@ -11,10 +12,11 @@ const db = firebase.firestore(firebase);
 export default function ListBirthday(props) {
     const { user } = props;
     const [showList, setShowList] = useState(true);
-
-    const [birthday, setBirthday] = useState({});
+    const [birthday, setBirthday] = useState([]);
+    const [pasatBirthday, setPasatBirthday] = useState([]);
 
     useEffect(() => {
+        setPasatBirthday([]);
         setBirthday([]);
         db.collection(user.uid)
             .orderBy("dateBirth", "asc")
@@ -26,9 +28,41 @@ export default function ListBirthday(props) {
                     data.id = doc.id;
                     itemsArray.push(data);
                 });
-                setBirthday(itemsArray);
+                formatData(itemsArray);
             });
     }, []);
+
+
+    const formatData = (items) => {
+        const currentData = moment().set({
+            hour: 0,
+            minute: 0,
+            second: 0,
+            millisecond: 0,
+        });
+        const birthdayTempArray = [];
+        const pasatBirthdayTempArray = [];
+        items.forEach((item) => {
+            const fechaCumple = new Date(item.dateBirth.seconds * 1000);
+            const fechaCumpleanos = moment(fechaCumple);
+            const currentYear = moment().get("year");
+            fechaCumpleanos.set({ year: currentYear });
+
+            const diffDate = currentData.diff(fechaCumpleanos, "days");
+            const itemTemp = item;
+            itemTemp.dateBirth = fechaCumpleanos;
+            itemTemp.days = diffDate;
+
+            if (diffDate <= 0) {
+                birthdayTempArray.push(itemTemp);
+            } else {
+                pasatBirthdayTempArray.push(itemTemp);
+            }
+
+            setBirthday(birthdayTempArray);
+            setPasatBirthday(pasatBirthdayTempArray);
+        });
+    }
 
     return (
         <View style={styles.container}>
